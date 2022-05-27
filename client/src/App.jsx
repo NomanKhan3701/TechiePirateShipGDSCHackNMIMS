c1d44b1d0a47e7f5ec62ee4cf5dc36d66aa44317
shreyash dhamane <shreyashb.dhamane0@gmail.com>
2022-05-27 18:12:35 +0530

    basic commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
-import {Routes, Route} from 'react-router'
-import './App.css'
+import { useState } from "react";
+import { Routes, Route } from "react-router";
+import "./App.css";
+import Login from "./pages/Login/Login";
+import SignUp from "./pages/SignUp/SignUp";
 
 function App() {
-
   return (
     <div className="App">
-      Hi i am app
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
