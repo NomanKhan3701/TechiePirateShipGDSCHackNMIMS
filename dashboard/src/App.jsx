733ad919c3e7440dc9c5a09f6509e01f04e00420
Noman <noman.khan3701@gmail.com>
2022-05-27 16:04:06 +0530

    Setup of router

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,8 +1,30 @@
 import { useState } from "react";
+import { Route, Routes } from "react-router";
+import Home from "../../client/src/pages/Home/Home";
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar"
+import AddItem from "./pages/AddItem/AddItem";
+import Ingredients from "./pages/Ingredients/Ingredients";
+import Login from "./pages/Login/Login";
+import Menu from "./pages/Menu/Menu";
+import Order from "./pages/Order/Order";
+import Signup from "./pages/Signup/Signup";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/order" element={<Order />} />
+        <Route path="/ingredients" element={<Ingredients />} />
+        <Route path="/additem" element={<AddItem />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ dashboard/src/pages/AddItem/AddItem.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './AddItem.scss'
+
+const AddItem = () => {
+  return (
+    <div>AddItem</div>
+  )
+}
+
+export default AddItem
