9edb0bfefcefb8653da4e5811502ff5b3ec15c98
Noman <noman.khan3701@gmail.com>
2022-05-28 16:08:29 +0530

    Merged with shreyash and made everything center

--- client/src/pages/Menu/Menu.jsx
+++ client/src/pages/Menu/Menu.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import Card from "../../components/Card/Card";
 import food1 from "../../assets/food1.jpg";
 import { IoMdAdd } from "react-icons/io";
@@ -6,8 +6,83 @@ import { Link } from "react-router-dom";
 import "./Menu.scss";
 
 const Menu = () => {
+  const categoryData = ["Veg", "Non-Veg", "Vegan"];
+  const availibilityData = ["Available", "Not-Available"];
+  const cuisineData = ["American", "Indian", "Chineese"];
+  const bestTimeToEatData = ["Breakfast", "Lunch", "Dinner", "Desert"];
+
+  const [foodData, setFoodData] = useState({
+    ItemName: "",
+    Image: "",
+    Category: "Veg",
+    Cuisine: "American",
+    BestTimeToEat: "Breakfast",
+    Price: "",
+    Availability: "Available",
+    Ingrediants: "",
+    Description: "",
+  });
+
+  const dropdownChange = (event) => {
+    const { name, value } = event.target;
+    setFoodData((prevValue) => {
+      return { ...prevValue, [name]: value };
+    });
+  };
+
   return (
-    <div className="menu">
+    <div className="menu-container">
+      <div className="filter-container">
+        <h1>Filter</h1>
+        <div className="categories">
+          <select
+            className="select-filter"
+            name="Category"
+            id="Category"
+            onChange={dropdownChange}
+          >
+            {categoryData.map((data) => {
+              return <option>{data}</option>;
+            })}
+          </select>
+        </div>
+        <div className="categories">
+          <select
+            className="select-filter"
+            name="Cuisine"
+            id="Cuisine"
+            onChange={dropdownChange}
+          >
+            {cuisineData.map((data) => {
+              return <option>{data}</option>;
+            })}
+          </select>
+        </div>
+        <div className="categories">
+          <select
+            className="select-filter"
+            name="BestTimeToEat"
+            id="BestTimeToEat"
+            onChange={dropdownChange}
+          >
+            {bestTimeToEatData.map((data) => {
+              return <option>{data}</option>;
+            })}
+          </select>
+        </div>
+        <div className="categories">
+          <select
+            className="select-filter"
+            name="Availibility"
+            id="Availibility"
+            onChange={dropdownChange}
+          >
+            {availibilityData.map((data) => {
+              return <option>{data}</option>;
+            })}
+          </select>
+        </div>
+      </div>
       <div className="card-container">
         <Card
           img={food1}
