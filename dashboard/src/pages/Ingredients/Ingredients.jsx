11d7156f18c04d273140a1c92993748c08725773
Noman <noman.khan3701@gmail.com>
2022-05-28 12:08:37 +0530

    Added ingredients

--- dashboard/src/pages/Ingredients/Ingredients.jsx
+++ dashboard/src/pages/Ingredients/Ingredients.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import './Ingredients.scss'
+import React from "react";
+import "./Ingredients.scss";
+import food2 from "../../assets/food2.jpg";
+import ingredient1 from "../../assets/ingredient1.jpg";
 
 const Ingredients = () => {
   return (
-    <div>Ingredients</div>
-  )
-}
+    <div className="ingredients-container">
+      <div className="ingredient">
+        <img src={ingredient1} alt="" />
+        <div className="info-div"></div>
+      </div>
+    </div>
+  );
+};
 
-export default Ingredients
+export default Ingredients;
