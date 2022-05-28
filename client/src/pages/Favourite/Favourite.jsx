a0e52ca50194acf36e6f4804921c9b7901675db8
Noman <noman.khan3701@gmail.com>
2022-05-28 15:23:27 +0530

    Added hover on favourite

--- client/src/pages/Favourite/Favourite.jsx
+++ client/src/pages/Favourite/Favourite.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import FavouriteCard from "../../components/FavouriteCard/FavouriteCard";
-import Card from "../../components/Card/Card";
 import food1 from "../../assets/food1.jpg";
 import "./Favourite.scss";
 
@@ -8,40 +7,47 @@ const Favourite = () => {
   return (
     <div className="menu">
       <div className="card-container">
-        <Card
+        <FavouriteCard
           img={food1}
           name="Avacado egg"
           desc=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex quos quasi obcaecati facilis. Esse mollitia impedit accusamus maiores molestiae ab exercitationem. Natus quas illum eum eaque cupiditate asperiores beatae iste. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex quos quasi obcaecati facilis. Esse mollitia impedit accusamus maiores molestiae ab exercitationem. Natus quas illum eum eaque cupiditate asperiores beatae iste."
           price={200}
           status="Out Of Stock"
         />
-        <Card
+        <FavouriteCard
           img={food1}
           name="Avacado egg"
           desc=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex quos quasi obcaecati facilis. Esse mollitia impedit accusamus maiores molestiae ab exercitationem. Natus quas illum eum eaque cupiditate asperiores beatae iste. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex quos quasi obcaecati facilis. Esse mollitia impedit accusamus maiores molestiae ab exercitationem. Natus quas illum eum eaque cupiditate asperiores beatae iste."
           price={200}
           status="Available"
         />
-        <Card
+        <FavouriteCard
           img={food1}
           name="Avacado egg"
           desc=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex quos quasi obcaecati facilis. Esse mollitia impedit accusamus maiores molestiae ab exercitationem. Natus quas illum eum eaque cupiditate asperiores beatae iste. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex quos quasi obcaecati facilis. Esse mollitia impedit accusamus maiores molestiae ab exercitationem. Natus quas illum eum eaque cupiditate asperiores beatae iste."
           price={200}
           status="Out Of Stock"
         />
-        <Card
+        <FavouriteCard
+          img={food1}
+          name="Avacado egg"
+          desc=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex quos quasi obcaecati facilis. Esse mollitia impedit accusamus maiores molestiae ab exercitationem. Natus quas illum eum eaque cupiditate asperiores beatae iste. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex quos quasi obcaecati facilis. Esse mollitia impedit accusamus maiores molestiae ab exercitationem. Natus quas illum eum eaque cupiditate asperiores beatae iste."
+          price={200}
+          status="Available"
+        />
+        <FavouriteCard
           img={food1}
           name="Avacado egg"
           desc=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex quos quasi obcaecati facilis. Esse mollitia impedit accusamus maiores molestiae ab exercitationem. Natus quas illum eum eaque cupiditate asperiores beatae iste. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex quos quasi obcaecati facilis. Esse mollitia impedit accusamus maiores molestiae ab exercitationem. Natus quas illum eum eaque cupiditate asperiores beatae iste."
           price={200}
           status="Out Of Stock"
         />
-        <Card
+        <FavouriteCard
           img={food1}
           name="Avacado egg"
           desc=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex quos quasi obcaecati facilis. Esse mollitia impedit accusamus maiores molestiae ab exercitationem. Natus quas illum eum eaque cupiditate asperiores beatae iste. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex quos quasi obcaecati facilis. Esse mollitia impedit accusamus maiores molestiae ab exercitationem. Natus quas illum eum eaque cupiditate asperiores beatae iste."
           price={200}
-          status="Out Of Stock"
+          status="Available"
         />
       </div>
     </div>
